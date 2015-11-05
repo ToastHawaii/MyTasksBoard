5e5f2d463c349fb72f89940a6e60865a770f7f8c
Markus Peloso <mpeloso@techgroup.ch>
2015-11-05 22:56:47 +0100

    support touch

--- js/MyTasksBoard.js
+++ js/MyTasksBoard.js
@@ -74,6 +74,25 @@ window.onload = function () {
     app = new App();
     app.start();
 };
+function touchHandler(event) {
+    var touch = event.changedTouches[0];
+    var simulatedEvent = document.createEvent("MouseEvent");
+    var asdf = {
+        touchstart: "mousedown",
+        touchmove: "mousemove",
+        touchend: "mouseup"
+    };
+    simulatedEvent.initMouseEvent(asdf[event.type], true, true, window, 1, touch.screenX, touch.screenY, touch.clientX, touch.clientY, false, false, false, false, 0, null);
+    touch.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
+}
+function init() {
+    document.addEventListener("touchstart", touchHandler, true);
+    document.addEventListener("touchmove", touchHandler, true);
+    document.addEventListener("touchend", touchHandler, true);
+    document.addEventListener("touchcancel", touchHandler, true);
+}
+init();
 var Services;
 (function (Services) {
     var Auth = (function () {
